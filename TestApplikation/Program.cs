6c18b9349f615a341c272d068aa5a07df6fb23bf
Kim <admin@AR-114-01>
2018-05-22 16:04:55 +0200

    init

--- TestApplikation/Program.cs
+++ TestApplikation/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace TestApplikation
 {
@@ -11,7 +8,45 @@ namespace TestApplikation
         static void Main(string[] args)
         {
 
-            //Leg løs her
+            Console.WriteLine("Hello World");
+
+            //simple variabler
+            System.Int32 i = 1;
+            string s = i.ToString();
+            Console.WriteLine(int.MinValue);
+            Console.WriteLine(int.MaxValue);
+
+            int ii;
+            ii = 100;
+
+            System.Boolean b = true;
+            bool bb = false;
+
+            //kommatal
+            double d;
+            System.Double dd;
+            d = 74.939838398;
+            Console.WriteLine(d.ToString("F4"));
+
+            //date
+            System.DateTime da;
+            da = System.DateTime.Now;
+            Console.WriteLine(da.ToString("yyyy-MM"));
+
+
+            //timespan
+            System.DateTime d1 = System.DateTime.Now;
+            System.DateTime d2 = new DateTime(2018, 12, 24);
+            System.TimeSpan ts = d2.Subtract(d1);
+            Console.WriteLine(ts.ToString());
+            Console.WriteLine(ts.TotalDays.ToString());
+
+            //konstanter
+            const double momsPct = 0.25 //bør defineres i en fil eller db
+
+            //modulus
+            
+
 
         }
     }
---
+++ Variabler/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variabler
+{
+    enum FilTyper
+    {
+        csv, pdf, txt
+            
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Pkt. 1
+            int heltal = 10;
+            heltal++;
+            heltal--;
+            heltal += 20;
+            Console.WriteLine(heltal.ToString());
+
+            //Pkt. 2
+            double kommatal = 12.5;
+            kommatal++;
+            kommatal--;
+            kommatal *= 2;
+            Console.WriteLine(kommatal.ToString());
+
+            //Pkt. 3 - enum defineret under namespace
+
+        FilTyper ft = FilTyper.csv;
+        Console.WriteLine(ft);
+        int r = (int)ft;
+        Console.WriteLine(r);
+
+            //Pkt. 4
+            DateTime dt = DateTime.Now;
+            Console.WriteLine(dt.ToString("ddMMyy"));
+            Console.WriteLine(dt.ToString("f"));
+            Console.WriteLine(dt.ToString("s"));
+
+            //Pkt. 5
+
+
+
+
+
+        }
+}
+}
