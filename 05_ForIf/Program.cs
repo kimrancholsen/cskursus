1f8224c12b532beaceec6bc74f556198451ff3e2
Kim <admin@AR-114-01>
2018-05-23 13:35:48 +0200

    for- og if-løkker

---
+++ 05_ForIf/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_ForIf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int i = 1; i < 11; i++)
+            {
+                for (int x = 1; x < 11; x++)
+                {
+                    string res = (i * x).ToString();
+                    if ((i * x) > 50)
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        else
+                        Console.ForegroundColor = ConsoleColor.White;
+                    
+                    Console.Write(res.PadLeft(4));
+                }
+                Console.WriteLine();
+            }
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
