4b81b3c48aa426f92ad0e0e590ce981d25dbb699
Kim <admin@AR-114-01>
2018-05-23 11:16:52 +0200

    arrays

---
+++ 03_Arrays/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] salaryArray = { 10000, 20000, 15000, 20000, 30000, 15000 };
+
+            //foreach
+            foreach (int i in salaryArray)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("Genemsnit " + salaryArray.Average());
+
+            //for løkke
+            double sum = 0;
+            for (int i = 0; i < salaryArray.Length; i++)
+            {
+                sum += salaryArray[i];
+            }
+
+            double gns = sum / salaryArray.Length;
+            Console.WriteLine("Gennemsnit " + gns.ToString("N2"));
+
+            //sort
+            System.Array.Sort(salaryArray);
+            foreach (int i in salaryArray)
+            {
+                Console.WriteLine(i);
+            }
+
+            //reverse
+            System.Array.Reverse(salaryArray);
+            Console.WriteLine("Reverted");
+            foreach (int i in salaryArray)
+            {
+                Console.WriteLine(i);
+            }
+
+        }
+    }
+}
---
+++ ReferenceTypes/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReferenceTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a1;
+            a1 = new int[5];
+
+            int[] a2 = new int[5];
+
+            a1[0] = 10;
+            a1[4] = 100;
+
+            a2 = a1;
+
+           
+        }
+    }
+}
