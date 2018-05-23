5a16d124c763273766f557aff47746b26eb5dadb
Kim <admin@AR-114-01>
2018-05-23 12:51:39 +0200

    strenge

---
+++ 04_Strenge/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_Strenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fornavn = "Mikkel";
+            string efternavn = "Cronberg";
+            string samletNavn = fornavn + " " + efternavn;
+            Console.WriteLine(samletNavn);
+
+            string navnStort = samletNavn.ToUpper();
+            Console.WriteLine(navnStort);
+
+            string navnLille = samletNavn.ToLower();
+            Console.WriteLine(navnLille);
+
+            string del = efternavn.Substring(0, 4);
+            Console.WriteLine(del);
+
+            string[] myStringArray = samletNavn.Split(' ');
+
+            for (int i = 0; i < myStringArray.Length; i++)
+            {
+                Console.WriteLine(myStringArray[i]);
+
+            }
+
+
+
+        }
+    }
+}
--- ReferenceTypes/Program.cs
+++ ReferenceTypes/Program.cs
@@ -10,6 +10,7 @@ namespace ReferenceTypes
     {
         static void Main(string[] args)
         {
+            //arrays
             int[] a1;
             a1 = new int[5];
 
@@ -20,7 +21,33 @@ namespace ReferenceTypes
 
             a2 = a1;
 
-           
+            //strenge
+            char t = 'A'; //char = enkelt karakter
+            string navn = "Kim";
+            navn = navn.ToUpper(); // Laver en ny streng variabel. Den oprindelige streng ændres ikke
+
+            string sti = @"c:\temp\test.txt";
+
+            Console.WriteLine(sti);
+
+            //csv assembly
+            string csv = "423;334;343;";
+            string[] aa = csv.Split(';');
+
+            // c# version 7
+            int alder = 20;
+            string person = "Kim";
+            string tekst = $"Personen {person} er {alder} år.";
+
+            //tilføje tekst til en streng over flere iterationer
+
+            System.Text.StringBuilder sb = new StringBuilder();
+            for (int i = 0;i<100_000; i++)
+            {
+                sb.Append("*");
+            }
+
+       
         }
     }
 }
