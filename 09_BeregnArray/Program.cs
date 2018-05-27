d018735a3956be9db41486aae9e357a36548c2f1
kiols <39522199+kiols@users.noreply.github.com>
2018-05-27 22:19:29 +0200

    BeregnArray opgave

---
+++ 09_BeregnArray/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09_BeregnArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] test = {10, 7, 6, 3, 1, 50, 3};
+            var res = BeregnOgSorterArray(test);
+        }
+
+        public static ArrayResultat BeregnOgSorterArray(int[] intarray)
+        {
+
+            ArrayResultat r;
+            r.sum = 0;
+
+            for (int i = 0; i < intarray.Length; i++)
+            {
+                r.sum += intarray[i];
+            }
+
+            r.gennemsnit = r.sum / intarray.Length;
+
+            System.Array.Sort(intarray);
+
+            return r;
+
+
+        }
+    }
+
+
+    struct ArrayResultat
+    {
+        public double sum;
+        public double gennemsnit;
+    }
+
+}
