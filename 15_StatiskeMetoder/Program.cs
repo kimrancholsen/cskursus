4f6ab92657f92ac2c75e7e30229e498c8d7d00a7
kimrancholsen <kimrancholsen@gmail.com>
2018-06-06 15:56:52 +0200

    egenskaber, statiske klasser

---
+++ 15_StatiskeMetoder/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15_StatiskeMetoder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ArealBeregninger.BeregnArealFirkat(10,10));
+            Console.WriteLine(ArealBeregninger.BeregnArealCirkel(10));
+        }
+    }
+
+    static class ArealBeregninger
+    {
+
+
+        public static double BeregnArealFirkat(double højde, double bredde)
+        {
+
+            return højde * bredde;
+
+        }
+
+        public static double BeregnArealCirkel(double radius)
+        {
+            return System.Math.PI * System.Math.Pow(radius,2);
+        }
+
+    }
+}
