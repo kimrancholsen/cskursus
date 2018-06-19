a7e0823cb84ca1c5c7159c5e7dc2dfba8ea3840b
kiols <kimrancholsen@gmail.com>
2018-06-19 15:56:42 +0200

    generic

---
+++ 27_GeneriskMetode/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _27_GeneriskMetode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 2;
+            int b = 4;
+            //Swap<int>(ref a, ref b);
+            Swap(ref a, ref b);
+
+            string x = "Kim";
+            string y = "Mette";
+            //Swap<string>(ref x, ref y);
+            Swap(ref x, ref y);
+
+
+        }
+
+        public static void Swap<T>(ref T a, ref T b)
+        {
+            T temp = a;
+            a = b;
+            b = temp;
+        }
+    }
+}
---
+++ 28_GeneriskKlasse/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _28_GeneriskKlasse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Personer1 pliste = new Personer1();
+            pliste.Tilføj(new Kursist());
+            //            Kursist k1 = new Kursist { Navn = "Kim", KursistId = 3543 };
+            Console.WriteLine(pliste.ToString());   
+        }
+    }
+
+
+    class Personer1
+    {
+        List<Person> typeList = new List<Person>();
+
+        public void Tilføj(Person p)
+        {
+            typeList.Add(p);
+        }
+    }
+
+    class Personer2<T>
+    {
+        List<T> genericList = new List<T>();
+
+        public void Tilføj(T p)
+        {
+            genericList.Add(p);
+        }
+
+    }
+    class Person
+    {
+        public string Navn { get; set; }
+    }
+
+    class Kursist : Person
+    {
+        public int KursistId { get; set; }
+    }
+
+    class Instruktør : Person
+    {
+        public int NøgleId { get; set; }
+    }
+}
