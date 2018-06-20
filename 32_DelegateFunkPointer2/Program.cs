5fa5e1b8c1e4603662d14a9f88a0e8c18f64fbb9
kiols <kimrancholsen@gmail.com>
2018-06-20 12:47:23 +0200

    Func og Action

---
+++ 32_DelegateFunkPointer2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _32_DelegateFunkPointer2
+{
+
+
+    class Program
+    {
+
+        //public delegate int DelegateBeregn(int x, int y);
+
+        static void Main(string[] args)
+        {
+            int res1 = Beregner(5, 6, Plus);
+            Console.WriteLine(res1.ToString());
+
+            int res2 = Beregner(100, 44, Minus);
+            Console.WriteLine(res2.ToString());
+
+            int res3 = Beregner(3, 3, Gange);
+            Console.WriteLine(res3.ToString());
+
+        }
+
+
+        public static int Beregner(int d, int e, Func<int, int, int> funktion)
+        {
+            return funktion(d, e);
+        }
+
+        public static int Plus(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Minus(int a, int b)
+        {
+            return a - b;
+        }
+
+        public static int Divider(int a, int b)
+        {
+            return a / b;
+        }
+
+        public static int Gange(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
+
---
+++ 33_DelegateFunkPointer3/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _33_DelegateFunkPointer3
+{
+
+    class Program
+    {
+
+        //public delegate int DelegateBeregn(int x, int y);
+
+        static void Main(string[] args)
+        {
+            int res1 = Beregner(10, 6, funktion: FindTilfældigFunktion() );
+            Console.WriteLine(res1.ToString());
+
+            int res2 = Beregner(100, 44, Minus);
+            Console.WriteLine(res2.ToString());
+
+            //int res3 = Beregner(3, 3, Gange);
+            //Console.WriteLine(res3.ToString());
+
+        }
+
+
+        public static Func<int, int, int> FindTilfældigFunktion()
+        {
+            if (DateTime.Now.Millisecond <= 250)
+                return Plus;
+            if (DateTime.Now.Millisecond <= 500)
+                return Minus;
+            if (DateTime.Now.Millisecond <= 750)
+                return Divider;
+            return Gange;
+                       
+        }
+
+        public static int Beregner(int d, int e, Func<int, int, int> funktion)
+        {
+            return funktion(d, e);
+        }
+
+
+        //regnefunktioner
+        public static int Plus(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Minus(int a, int b)
+        {
+            return a - b;
+        }
+
+        public static int Divider(int a, int b)
+        {
+            return a / b;
+        }
+
+        public static int Gange(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
