73f353ec005f7cfc9ba474356d73070540859e2f
kiols <kimrancholsen@gmail.com>
2018-06-20 13:32:30 +0200

    Lambda

---
+++ 34_Lambda/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _34_Lambda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> a = new List<int>() { 5, 1, 51, 7, 1, 56, 36, 5 };
+            Console.WriteLine(a.FindIndex(Find));
+            Console.WriteLine();
+            Console.WriteLine(a.FindIndex((i) => { return i == 51; }));
+            Console.WriteLine();
+            Console.WriteLine(a.FindIndex(i => i == 51));
+
+
+            Console.WriteLine("****");
+            a.ForEach(ListVærdi);
+            Console.WriteLine("....");
+            a.ForEach((i) => { Console.WriteLine(i); });
+            Console.WriteLine("----");
+            a.ForEach((i) => Console.WriteLine(i));
+
+
+
+
+
+
+        }
+
+        static void ListVærdi(int i)
+        {
+            Console.WriteLine(": " + i);
+        }
+
+        static bool Find(int i)
+        {
+            return i == 51;
+        }
+    }
+}
